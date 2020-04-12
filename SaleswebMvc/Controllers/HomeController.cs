984cdae8f2d4ab0c30d06aa028000cace1a83162
AlanisMayumi <mayumi.alanis@gmail.com>
2020-04-12 15:32:00 -0300

    department list

--- SaleswebMvc/Controllers/HomeController.cs
+++ SaleswebMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SaleswebMvc.Models;
+using SaleswebMvc.Models.ViewModels;
 
 namespace SaleswebMvc.Controllers
 {
